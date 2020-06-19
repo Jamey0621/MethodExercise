400dc41745ca2121bcb3e93fd7ae28902891eba9
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-06-19 12:58:18 -0500

    MethodExercise

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -6,7 +6,16 @@ namespace MethodExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"What is your name?");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"Your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{myName} was outside playing with friends. Then they seen a big {color} {animal} and named it Fluffy.");
         }
     }
 }
